7dbff56ec6f150bd2d547de343e2950b1c895380
Kevin <kevin.ho@edu.devinci.fr>
2021-10-14 16:24:30 +0200

    add javascript

---
+++ script.js
@@ -0,0 +1,16 @@
+const slidingNewsletter = document.querySelector('.slide-in');
+
+window.addEventListener('scroll', () => {
+
+    const {scrollTop, clientHeight} = document.documentElement;
+
+    // console.log(scrollTop, clientHeight);
+
+    const topElementToTopViewport = slidingNewsletter.getBoundingClientRect().top;
+
+    console.log(topElementToTopViewport);
+
+    if(scrollTop > (scrollTop + topElementToTopViewport).toFixed() - clientHeight){
+        slidingNewsletter.classList.add('active')
+    }
+})
